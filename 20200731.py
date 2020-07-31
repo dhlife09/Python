0ff4b479ed25e92fe24e9834b30d637960e6d103
gpb8902 <gpb8902@gmail.com>
2020-07-31 22:50:22 +0900

    Update 20200731.py

--- 20200731.py
+++ 20200731.py
@@ -15,3 +15,26 @@ print(9<=9)
 print(20 <=-30)
 print((20>19) & (30<10000))
 print((5>0) or (20<21))
+print(2 + 3 * 8)
+print((2 +3) * 8)
+number = 2**2
+print(number)
+number = (number + 7) * 4
+print(number)
+number = number/11
+print(number)
+number = number **4
+print(number)
+number = number/2
+print(number)
+print(abs(-6488))
+print(abs(56545) - abs(6545))
+print(round(7.68468))
+print(pow(9, 9))
+print(max(2, 9))
+print(min(68, 6544))
+
+from math import *
+print(floor(4.99))
+print(ceil(5.65498))
+print(sqrt(16))
