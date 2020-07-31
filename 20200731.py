75fafd66fec82f60fdb8b199c01bf88582854694
Dohyun Park <dhlife09@gmail.com>
2020-07-31 19:53:10 +0900

    Create 20200731.py

---
+++ 20200731.py
@@ -0,0 +1,17 @@
+print(1+2+3)
+print(-2+66)
+print(2**10)
+print(6%4) 
+print(5 == 3-(-9))
+print(3**4)
+print(9//5)
+if ('C' == 'C'):
+    print('You are right!')
+else:
+        print('Are You stupid?')
+
+print(29<-100)
+print(9<=9)
+print(20 <=-30)
+print((20>19) & (30<10000))
+print((5>0) or (20<21))
